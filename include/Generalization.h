52e796bcde886afb7367a138bb5db4f97ea87bbc
mzh19940817 <1142587688@qq.com>
2021-08-15 05:32:10 +0800

    add generalization

--- include/Generalization.h
+++ include/Generalization.h
@@ -1,7 +1,33 @@
 #ifndef GENERALIZATION_H_
 #define GENERALIZATION_H_
 
+#include <string>
 
+struct Person
+{
+    Person(const std::string& name) : name(name) { }
 
+    const std::string& getName() const
+    {
+        return this->name;
+    }
+
+private:
+    std::string name;
+};
+
+struct Student : Person
+{
+    Student(const std::string& name, const int number)
+            : Person(name), number(number) { }
+
+    const int getNumber() const
+    {
+        return this->number;
+    }
+
+private:
+    int number;
+};
 
 #endif /* GENERALIZATION_H_ */
--- test/GeneralizationTest.cpp
+++ test/GeneralizationTest.cpp
@@ -1,2 +1,15 @@
+#include "Generalization.h"
+#include "gtest/gtest.h"
 
+struct GeneralizationTest : testing::Test
+{
+
+};
+
+TEST_F(GeneralizationTest, should_be_true_when_give_student)
+{
+    Student stu{"Jack", 1025};
+    ASSERT_EQ("Jack", stu.getName());
+    ASSERT_EQ(1025, stu.getNumber());
+}
 
