2c8e18e8588746859946858a62cb44add7f31d0b
mzh19940817 <1142587688@qq.com>
2021-08-14 21:37:00 +0800

    add composition

--- test/CompositionTest.cpp
+++ test/CompositionTest.cpp
@@ -6,18 +6,19 @@ struct CompositionTest : testing::Test
     void SetUp() override
     {
         Point point;
-        creature.setCreature("dog", point);
+        creature.setCreature(this->name, point);
     }
 
     void assertCreature(const int x, const int y)
     {
-        ASSERT_EQ("dog", creature.getName());
-        ASSERT_EQ(x, creature.getXLocation());
-        ASSERT_EQ(y, creature.getYLocation());
+        ASSERT_EQ(this->name, this->creature.getName());
+        ASSERT_EQ(x, this->creature.getXLocation());
+        ASSERT_EQ(y, this->creature.getYLocation());
     }
 
 protected:
     Creature creature;
+    std::string name{"dog"};
 };
 
 TEST_F(CompositionTest, should_be_ture_location_when_move_to_x1_y2)
