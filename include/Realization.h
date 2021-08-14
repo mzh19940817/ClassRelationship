a03273658b9cb0af49ce48e0c88400f43f028da0
mzh19940817 <1142587688@qq.com>
2021-08-15 05:48:18 +0800

    add realization

--- include/Realization.h
+++ include/Realization.h
@@ -1,7 +1,29 @@
 #ifndef REALIZATION_H_
 #define REALIZATION_H_
 
+#include <iostream>
 
+struct Animal
+{
+    virtual ~Animal() {}
 
+    virtual std::string move() const = 0;
+};
+
+struct Dog : Animal
+{
+    std::string move() const override
+    {
+        return "run";
+    }
+};
+
+struct Bird : Animal
+{
+    std::string move() const override
+    {
+        return "fly";
+    }
+};
 
 #endif /* REALIZATION_H_ */
--- test/RealizationTest.cpp
+++ test/RealizationTest.cpp
@@ -1,2 +1,17 @@
+#include "Realization.h"
+#include <memory>
+#include "gtest/gtest.h"
 
+struct RealizationTest : testing::Test
+{
 
+};
+
+TEST_F(RealizationTest, should_be_true_when_give_animal)
+{
+    std::unique_ptr<Animal> dog{new Dog};
+    ASSERT_EQ("run", dog->move());
+
+    std::unique_ptr<Animal> bird{new Bird};
+    ASSERT_EQ("fly", bird->move());
+}
